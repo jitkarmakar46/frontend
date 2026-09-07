08d6526a8e24571118a50b5aa2ef32d4af8deacf
jitkarmakar46 <jitkarmakar46@gmail.com>
2026-09-07 18:43:28 +0530

    Update AdminDashboard.jsx

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -39,7 +39,7 @@ export default function AdminDashboard() {
     }, [token]);
 
     useEffect(() => {
-        if (token) {
+         useEffect(() => {      if (token) {          fetchAppointments();      }  }, [token, fetchAppointments]);
             fetchAppointments();
         }
     }, [token, fetchAppointments]);
